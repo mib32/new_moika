ff129d3163080c2a2b5649dcc90e744cc627a69f
Anton Murygin <mibus32@gmail.com>
2014-08-26 12:22:32 +0400

    uni

--- config/deploy.rb
+++ config/deploy.rb
@@ -68,3 +68,42 @@ namespace :deploy do
   end
 
 end
+
+namespace :unicorn do
+  desc 'Stop Unicorn'
+  task :stop do
+    on roles(:app) do
+      if test("[ -f #{fetch(:unicorn_pid)} ]")
+        execute :kill, capture(:cat, "#{current_path}/tmp/pids/unicorn.pid")
+      end
+    end
+  end
+
+  desc 'Start Unicorn'
+  task :start do
+    on roles(:app) do
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, "exec unicorn -c #{fetch(:unicorn_config)} -D"
+        end
+      end
+    end
+  end
+
+  desc 'Reload Unicorn without killing master process'
+  task :reload do
+    on roles(:app) do
+      if test("[ -f #{fetch(:unicorn_pid)} ]")
+        execute :kill, '-s USR2', capture(:cat, fetch(:unicorn_pid))
+      else
+        error 'Unicorn process not running'
+      end
+    end
+  end
+
+  desc 'Restart Unicorn'
+  task :restart
+  before :restart, :stop
+  before :restart, :start
+end
+
