12bb468ee12c586201efe1caede027eff5b8aa6d
Anton Murygin <mibus32@gmail.com>
2014-08-28 19:17:31 +0400

    active in model

--- app/models/nav.rb
+++ app/models/nav.rb
@@ -3,4 +3,8 @@ class Nav < ActiveRecord::Base
   validates :image_url, allow_blank: true, format: {
     with:    %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for GIF, JPG or PNG image.' }
+
+  def active
+    path.tr('/','') == params[:controller]
+  end
 end
