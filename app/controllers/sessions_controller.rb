0339e6a53602e91fc96292e2bd282e3b93f1884a
Anton Murygin <mibus32@gmail.com>
2014-09-11 11:44:58 +0400

    new user model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,68 +2,32 @@ class SessionsController < Devise::SessionsController
 	layout false
   before_filter :resource_name
 
-  def resource_name
-    # if params[:user].present?
-    #   email = params[:user][:email]
-    #   @user_test ||= User.where(email: email)
-    #   @normal_user_test ||= NormalUser.where(email: email)
-    #   if @user_test.any?
-    #     res = :user
-    #   elsif @normal_user_test.any?
-    #     res = :normal_user
-    #   else
-    #     res = :user
-    #   end
-    # else
-    #   res = :user
-    # end
-    # resa = :normal_user
-    # logger.info res
-    # logger.info resa
-    # return resa
-    # :normal_user
-    # _normal_or_car_wash_admin
-    # logger.info devise_mapping.name
-    devise_mapping.name
-  end
+  # def resource_name
+  #   devise_mapping.name
+  # end
 
   def new
     self.resource = resource_class.new(sign_in_params)
-    @d = auth_options
-    # @normal_user = NormalUser.new(sign_in_params)
     clean_up_passwords(resource)
+    # respond_with(resource, serialize_options(resource))
     respond_to do |format|
       format.js
       format.html
     end
   end
 
-  def get_user
-    respond_to do |format|
-      format.json { render :json => {:scope => _normal_or_car_wash_admin(params[:email])} }
-    end
-  end
-
-  private
-
-  def _normal_or_car_wash_admin email
-      user_count = User.where(email: email).count
-      normal_user_count = NormalUser.where(email: email).count
-      if user_count > 0
-        res = :user
-      elsif normal_user_count > 0
-        res = :normal_user
-      else
-        res = :user
-      end
-
-      return res
-  end
+  # def get_user
+    # respond_to do |format|
+      # format.json { render :json => {:scope => _normal_or_car_wash_admin(params[:email])} }
+    # end
+  # end
+ 
   # def create
-  #   if resource_name = :normal_user
-  #     params[:normal_user] = params[:user]
-  #     params[:user] = {:test => true}
-  #   end
-  #   super
+  #   self.resource = warden.authenticate!(auth_options)
+  #   set_flash_message(:notice, :signed_in) if is_flashing_format?
+  #   byebug
+  #   sign_in(resource, resource)
+  #   yield resource if block_given?
+  #   respond_with resource, location: after_sign_in_path_for(resource)
   # end
 end
