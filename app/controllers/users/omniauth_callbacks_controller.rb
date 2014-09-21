07278b46d4af670d73a4dcf7ccd4ca19168d032f
Anton Murygin <mibus32@gmail.com>
2014-09-21 17:33:09 +0400

    omniauth

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,31 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def facebook
+    access_token = request.env["omniauth.auth"]
+    @user = User.find_for_facebook_oauth access_token
+    if @user
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
+      sign_in @user, :event => :authentication
+      @sign_up_successful = true
+    else
+      @user = User.new(:social_id => access_token.uid,
+                  :provider => access_token.provider,
+                   :url => access_token.info.urls.Facebook,
+                   :name => access_token.extra.raw_info.name,
+                   :nickname => access_token.extra.raw_info.username, 
+                   :email => access_token.extra.raw_info.email, 
+                   :type => 'user',
+                   :password => Devise.friendly_token[0,20]) 
+      if @user.save
+        flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
+        sign_in @user, :event => :authentication
+        @sign_up_successful = true
+      else
+        flash[:notice] = 'Зарегистрироваться не удалось:'
+        render 'devise/sessions/new'
+      end
+    end
+  end
+
+  def vkontakte
+  end
+end
