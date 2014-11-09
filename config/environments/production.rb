77b40d65d394931e9f2770be8953aeab2b0da126
Anton Murygin <mibus32@gmail.com>
2014-11-09 14:37:11 +0400

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,8 @@ Moika::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_mailer.default_url_options = { :host => 'http://moika-77.ru' }
+
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
